bb8b29bb0baa17a7a5652c5d0b426ce22dc9ced1
Glurp <ktkellerbusiness@gmail.com>
2025-09-21 20:32:48 -0400

    Created foundation for Pages and First Screen
    Installed React router dom. Created directory Pages and Search page which will be the first for presentation Monday

---
+++ src/Pages/Search.jsx
@@ -0,0 +1,12 @@
+import "./Search.css"
+
+export function Search(){
+
+    return(
+        <>
+            <div className={"SearchHead"}>Something
+                <h1>Hi there, Sankalp</h1>
+            </div>
+        </>
+    )
+}
