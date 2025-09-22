2fdebd6c87e186b37663909165ca3e2c15646e0e
Glurp <ktkellerbusiness@gmail.com>
2025-09-21 22:04:03 -0400

    Font updated. Page Forming
    Background color was changed, and century gothic was imported into the src/fonts.  Formatting of CSS and standards are being formed

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -2,15 +2,37 @@ import "./Search.css"
 
 export function Search(){
 
+    function PageButtons() {
+        return(
+            <>
+                <button>
+                    About
+                </button>
+                <button>
+                    Resources
+                </button>
+                <button>
+                    News
+                </button>
+                <button>
+                    Events
+                </button>
+                <button>
+                    Contact
+                </button>
+                <button className={"Donate"}>
+                    Donate
+                </button>
+            </>
+        )
+    }
     return(
         <>
             <div className={"SearchHead"}>
-                <h1>Hi there, Sankalp</h1>
+                <h1>Georgia Watch</h1>
 
                 <div className={"buttonset"}>
-                    <button>
-                        About
-                    </button>
+                    <PageButtons/>
                 </div>
             </div>
         </>
