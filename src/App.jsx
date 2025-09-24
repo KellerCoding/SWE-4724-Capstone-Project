b2d91a38b116ce0110d05b66449d2e45f07c9d1f
YetronLives <cnnorteyjr@gmail.com>
2025-09-24 17:02:36 -0400

    completed carousel

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,22 @@ import { Navbar } from "./Pages/Navbar.jsx"
 import { Search } from "./Pages/Search.jsx"
 import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import { SubNavbar } from "./Pages/SubNavbar.jsx"
-
+import { Homepage } from "./Pages/Homepage.jsx"
 
 function App() {
 
-
   return (
-    
     <Router>
         <div width="100%" style={{ backgroundColor: "#fff" }}>
             <Navbar/>
             <SubNavbar/>
             <Routes>
-                {/* <Route path="/" element={<Search/>}/> */}
+                <Route path="/" element={<Homepage/>}/>
+                {/* <Route path="/homepage" element={<Homepage/>}/> */}
+                {/* <Route path="/search" element={<Search/>}/> */}
             </Routes>
         </div>
     </Router>
-
 )
 }
 
