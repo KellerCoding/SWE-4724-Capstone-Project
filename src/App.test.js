0f404541d92d8f6c3b42f6cffab75384c73491a9
Leneord1 <71476576+Leneord1@users.noreply.github.com>
2025-10-08 22:12:34 -0400

    Delete src directory
    Wanted to push fresh code and found it easier to delete the src directory and push the latest code.

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import ReactDOMClient from 'react-dom/client';
-import App from './App';
-
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOMClient.createRoot(div).render(<App />);
-});
