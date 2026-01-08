5232f7a60e9b306252e2fb8747599cc3565acad0
YetronLives <cnnorteyjr@gmail.com>
2026-01-08 11:30:12 -0500

    back to  homepage

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -2,8 +2,9 @@ import "./Navbar.css"
 export function Navbar(){
     return (
         <nav className="navbar">
+            <div className="back-to-gwatch"><a href="https://georgiawatch.org/">Back to Georgia Watch</a></div>
             <div className="navbar-brand">
-                Protecting Consumers Since 2002
+               <strong>PROTECTING CONSUMERS SINCE 2002 </strong> 
             </div>
         </nav>
     )
