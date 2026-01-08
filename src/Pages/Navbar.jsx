9ffbca0ad9ca35dcbb63338dbec7ee03da1b6c19
YetronLives <cnnorteyjr@gmail.com>
2026-01-07 23:53:52 -0500

    post meeting update

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -3,28 +3,7 @@ export function Navbar(){
     return (
         <nav className="navbar">
             <div className="navbar-brand">
-                PROTECTING CONSUMERS SINCE
-            </div>
-            <label htmlFor='menu' tabIndex="0">
-               ☰
-            </label>
-            <input id='menu' type='checkbox' />
-            <ul className="navbar-nav">
-                <li className="nav-item">
-                    <a href="" className="nav-link">ABOUT</a>
-                </li>
-                <li className="nav-item">
-                    <a href="" className="nav-link">MEDIA</a>
-                </li>
-                <li className="nav-item">
-                    <a href="" className="nav-link">CONTACT</a>
-                </li>
-            </ul>
-            <div className="search-container">
-                <div className="search-box">
-                    <span className="search-icon">🔍</span>
-                    <input type="text" placeholder="Search..." className="search-input" />
-                </div>
+                Protecting Consumers Since 2002
             </div>
         </nav>
     )
