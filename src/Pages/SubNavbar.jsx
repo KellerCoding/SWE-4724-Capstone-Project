59e87e702017449ec1f06e462284ea3eb5e3208c
YetronLives <cnnorteyjr@gmail.com>
2026-01-09 09:47:53 -0500

    Add navigation buttons to GeorgiaMap and HospitalScore pages
    - Introduced a back button in both GeorgiaMap and HospitalScore components for easier navigation to the scorecard.
    - Updated CSS for navigation buttons to enhance styling and user experience.
    - Refactored layout to accommodate new navigation elements while maintaining existing functionality.

--- src/Pages/SubNavbar.jsx
+++ src/Pages/SubNavbar.jsx
@@ -19,9 +19,6 @@ export function SubNavbar(){
                         <button onClick={redirectToGAWatch} type="button">
                             <img style={{width:"10rem"}} src={gwatchLogo} alt="Georgia Watch Logo" />
                         </button>
-                        <Link to={"/scorecard"} type="button">
-                            <button>Back To Scorecards</button>
-                        </Link>
                     </div>
                 </div>
             </div>
